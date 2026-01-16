e9e422ea4149b2a3bd3626240d9f2353e60d97be
JuliaKeller13 <juliakeller.vk@gmail.com>
2026-01-16 12:03:13 +0100

    ifo apdate

--- scripts/templates.js
+++ scripts/templates.js
@@ -3,9 +3,13 @@ function getBooksHtml(indexBook) {
     <div class="book-card" tabindex="0">
       <h3>${books[indexBook].title}</h3>
       <img src="${books[indexBook].cover}" alt="${books[indexBook].title}" class="cover-img">
-      <p class="book-info">Autor: ${books[indexBook].author}</p>
+      <div class="book-info-section">
+        <p class="book-info">Autor: ${books[indexBook].author}</p>
+        <p>Erschien: ${books[indexBook].publishedYear}</p>
+        <p>Genre: ${books[indexBook].genre}</p>
+      </div>
       <div class="like-and-price">
-        <p class="price">${books[indexBook].price}€</p>
+        <p class="price">${books[indexBook].price.toFixed(2)}€</p>
         <div class="like-amount">
           <button id="likeButton" class="like-button" onClick="like()">
             <img src="./assets/icons/heart.png" alt="like" style="width:22px"/>
