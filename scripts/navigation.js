10b1d7666bbbe41e9466802887837bd9ac8a7e60
JuliaKeller13 <juliakeller.vk@gmail.com>
2026-01-20 12:54:23 +0100

    not finished code deleted

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,21 +1,3 @@
-// //ham menu
-// const hamMenu = document.querySelector(".ham-menu");
-// const offScreenMenu = document.querySelector(".off-screen-menu");
-      
-// hamMenu.addEventListener("click", function () {
-//   hamMenu.classList.toggle("active");
-//   offScreenMenu.classList.toggle("active");
-// });
-
-// //activ page
-// const currentPage = window.location.pathname;
-
-// document.querySelectorAll("hauptmenu a").forEach(link => {
-//     if (link.href.includes(currentPage)) {
-//         link.classList.add("active");
-//     }
-// });
-
 //Keyboard fuction global
 function handleKey(event, action) {
   if (event.key === "Enter" || event.key === " ") {
